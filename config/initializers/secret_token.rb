60b22dc72cc594b3bc632fc08e7e070cd89faa72
Bacancy <binal.patel@bacancytechnology.com>
2012-09-26 09:24:37 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chitchat::Application.config.secret_token = 'fb2419a6c3dd100db69080853f4682632088e35f304cdc9c91935d2e72e5ccfa28ec1af10c4f0e8ba02efeee6aefcc0ee294cf829b723f842307467bb6e25b74'
