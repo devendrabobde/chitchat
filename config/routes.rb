eba731438f6b069207e05ce304f2a9ee2a62dee3
Bacancy <binal.patel@bacancytechnology.com>
2012-09-26 09:47:10 +0530

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Chitchat::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
