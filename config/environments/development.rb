71990b21b6eb05d01e80bbd4649e1e82a4975dde
Bacancy <binal.patel@bacancytechnology.com>
2012-10-01 10:09:22 +0530

    Config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Chitchat::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.perform_deliveries = true
   # Other config items omitted.
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  #config.action_mailer.default_url_options = { :host => "localhost:3000" }
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :sendmail
   # config.action_mailer.sendmail_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Chitchat::Application.configure do
   
    # Other config items omitted.
   
-  config.action_mailer.default_url_options = { :host => "http://friendsconnect.herokuapp.com" }
-  config.perform_deliveries=true
+  # config.action_mailer.default_url_options = { :host => "http://friendsconnect.herokuapp.com" }
+  # config.perform_deliveries=true
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
@@ -83,19 +83,19 @@ Chitchat::Application.configure do
     # :host => "http://friendsconnect.herokuapp.com"
   # }
   
-  config.action_mailer.default_url_options = { :host => 'friendsconnect.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "friendsconnect.herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["devendrabobde89@gmail.com"],
-  password: ENV["deva123456"]
-}
+  # config.action_mailer.default_url_options = { :host => 'friendsconnect.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.smtp_settings = {
+  # address: "smtp.gmail.com",
+  # port: 587,
+  # domain: "friendsconnect.herokuapp.com",
+  # authentication: "plain",
+  # enable_starttls_auto: true,
+  # user_name: ENV["devendrabobde89@gmail.com"],
+  # password: ENV["deva123456"]
+# }
   
 end
