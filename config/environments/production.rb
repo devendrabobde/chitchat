7298c9589830544003294a57c441dbe8a975a0d3
Bacancy <binal.patel@bacancytechnology.com>
2012-10-01 07:48:53 +0530

    Changed Config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Chitchat::Application.configure do
   
    # Other config items omitted.
   
-  config.action_mailer.default_url_options = { :host => "https://friendsconnect.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "http://friendsconnect.herokuapp.com" }
   config.perform_deliveries=true
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -80,6 +80,6 @@ Chitchat::Application.configure do
   }
 
   config.action_mailer.default_url_options = {
-    :host => "https://friendsconnect.herokuapp.com"
+    :host => "http://friendsconnect.herokuapp.com"
   }
 end
